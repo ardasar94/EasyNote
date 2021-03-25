a6eda6f68a0da996175d31ac90c2ed48aba7f523
ardasar94 <sarihanarda@gmail.com>
2021-03-25 12:23:49 +0300

    Identity GetDisplayName Extension

---
+++ EasyNote/Helpers/IdentityHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace EasyNote.Helpers
+{
+    public static class IdentityHelpers
+    {
+        public static string GetDisplayName(this IIdentity identity)
+        {
+            return ((ClaimsIdentity)identity).FindFirst("DisplayName")?.Value;
+        }
+    }
+}
