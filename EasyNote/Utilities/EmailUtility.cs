567a40c60a2f150e126590eb9d903ab33a93a083
ardasar94 <sarihanarda@gmail.com>
2021-03-30 10:20:33 +0300

    Google Auth

--- EasyNote/Utilities/EmailUtility.cs
+++ EasyNote/Utilities/EmailUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -12,9 +13,13 @@ namespace EasyNote.Utilities
     {
         public static async Task SendEmailAsync(string to, string subject, string body)
         {
-            using (var smtp = new SmtpClient("ardasarihan.com"))
+            using (var smtp = new SmtpClient())
             {
+                smtp.Credentials = new NetworkCredential(
+                    ConfigurationManager.AppSettings["SmtpUserName"],
+                    ConfigurationManager.AppSettings["SmtpPassword"]
 
+                    );
                 MailMessage message = new MailMessage("easynote@ardasarihan.com", to, subject, body);
                 message.IsBodyHtml = true;
                 await smtp.SendMailAsync(message);
